de26ed15da249df1c92451654c5ac59526f5b3d3
Dennys Marcos <kardeenx@gmail.com>
2020-05-31 19:44:49 -0300

    node imagePath correction

--- script.js
+++ script.js
@@ -390,7 +390,7 @@ async function initMap() {
     })
         
     var markerCluster = new MarkerClusterer(map, markers,
-        {imagePath: 'node_modules/@google/markerclustererplus/images/m'});
+        {imagePath: './node_modules/@google/markerclustererplus/images/m'});
     
 
     var marker = new google.maps.Marker({
