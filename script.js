a43adb134a7aea7a474c4cc004c37fd219076878
Dennys Marcos <kardeenx@gmail.com>
2020-05-31 15:00:38 -0300

    bones done

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 //Global variable declarations
 var map, heatmap;
-var user = {
-    name: "user",
-    lat: 0,
-    lng: 0,
-    location: {lat: 0, lng: 0}
-}
+var user = { name: "user", location: {lat: 0, lng: 0}};
 
 
-// Function calls
-getLocation();
+// Suppose this is the application database data of users online, or something like that.
+// Probably the best scenario would be drawing only what is at user screen to avoid server overloading with multiple cross operations between users requesting data and sending their location.
+var dbl = {
+    client: {location: user.location}
+};
 
-// TODO Draw information about localization on-screen
-
-// Geolocation parsing
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(extract);
-        console.log("Geolocation parsed.");
-        console.log("user.location.Lat: " + user.location.lat +" Lng: " +user.location.lng);
-        // This line below makes sure we have a global location object to be used within the program with client updates and movement tracking - again, not sure if it is the ideal way to manage this kind of data.
-        updateLocation();
-    } else {
-        alert("Geolocation is not supported by this browser.");
+// Testing purposes - database overflow with random ordered users
+// Should be execute only once per runtime, as it does not detect multiple "same-name" users - tho it generates random numbers - very unlikely to repeat any.
+async function objOverflow(database, value){
+    await sleep(2000);
+    var newUser = {};
+    for (i = 1; i < value; i++) {
+        
+        var newUserName = "user" + i
+        newUser = {};
+        newUser[newUserName] = {location: {lat: randomizeCoords(user.location.lat), lng: randomizeCoords(user.location.lng)}};
+                
+        var assign = Object.assign(database, newUser);
     }
-    setTimeout(getLocation, 5000);
 }
 
 // Function called by the method getCurrrentPosition which stores user location inside this global variable - not sure if it is the best way to work with this kind of data.
@@ -33,53 +30,392 @@ function extract(position) {
     user.location.lng = position.coords.longitude;
 }
 
-// TODO This function is supposed to handle user location packets and send them away to a database where it can be shown at the map to other users.
-function updateLocation() {
-    // send.to.database(user.name, userLocation);
-    console.log("Client location update sent to db.")
-}
-
-function receiveLocation() {
-
-}
-
-// TODO Function is meant to be updating the client user interface with the data it receives from the server
-function updateView() {
-    map.setCenter(user.location);
-    setTimeout(updateView, 2000);
-    //console.log("View updated");
-}
-
-
-// Map initialization
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: user.location
-    });
-
-    let infoWindows = new google.maps.InfoWindow;
-
-    // Suppose this is the application database data of users online, or something like that.
-    // Probably the best scenario would be drawing only what is at user screen to avoid server overloading with multiple cross operations between users requesting data and sending their location.
-    var dbMarkers = {
-        user1: {lat: user.lat, lgn: user.lng},
-        user2: {lat: randomizeCoords(user.lat), lgn: randomizeCoords(user.longitude)},
-
+// Geolocation parsing
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(extract);
+        // console.log("Geolocation parsed.");
+        // console.log("user.location.Lat: " + user.location.lat +" Lng: " +user.location.lng);
+        // This line below makes sure we have a global location object to be used within the program with client updates and movement tracking - again, not sure if it is the ideal way to manage this kind of data.
+    } else {
+        alert("Geolocation is not supported by this browser.");
     }
-    
+    setTimeout(getLocation, 5000);
 }
 
-// Simple function meant to give a little spread to coordinates point - for emulation and testing
+// Give a little spread to coordinates - for emulation and testing
 function randomizeCoords(value) {
     var rand = Math.random();
     var luck = Math.random();
 
     if (luck > 0.50) {
-        value += rand/100;
+        value += rand / 150;
     } else {
-        value -= rand/100;
+        value -= rand / 150;
     }
+    return parseFloat(value.toFixed(7));
+}
 
-    return value;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function initMap() {
+    getLocation();
+    objOverflow(dbl, 200);
+
+    await sleep(4000);
+
+    var icons = {
+        client: {
+            url: '/headicon.png',
+            scaledSize: new google.maps.Size(45, 45)
+        },
+        cluster: {
+            url: 'testicon2.png',
+            scaledSize: new google.maps.Size(50,50)
+        }
+    };
+
+    var styledMapLight = new google.maps.StyledMapType(
+        [
+            {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+                {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+                {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+                {
+                featureType: 'administrative',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#c9b2a6'}]
+                },
+                {
+                featureType: 'administrative.land_parcel',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#dcd2be'}]
+                },
+                {
+                featureType: 'administrative.land_parcel',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#ae9e90'}]
+                },
+                {
+                featureType: 'landscape.natural',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+                },
+                {
+                featureType: 'poi',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+                },
+                {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#93817c'}]
+                },
+                {
+                featureType: 'poi.park',
+                elementType: 'geometry.fill',
+                stylers: [{color: '#a5b076'}]
+                },
+                {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#447530'}]
+                },
+                {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#f5f1e6'}]
+                },
+                {
+                featureType: 'road.arterial',
+                elementType: 'geometry',
+                stylers: [{color: '#fdfcf8'}]
+                },
+                {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#f8c967'}]
+                },
+                {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#e9bc62'}]
+                },
+                {
+                featureType: 'road.highway.controlled_access',
+                elementType: 'geometry',
+                stylers: [{color: '#e98d58'}]
+                },
+                {
+                featureType: 'road.highway.controlled_access',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#db8555'}]
+                },
+                {
+                featureType: 'road.local',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#806b63'}]
+                },
+                {
+                featureType: 'transit.line',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+                },
+                {
+                featureType: 'transit.line',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#8f7d77'}]
+                },
+                {
+                featureType: 'transit.line',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#ebe3cd'}]
+                },
+                {
+                featureType: 'transit.station',
+                elementType: 'geometry',
+                stylers: [{color: '#dfd2ae'}]
+                },
+                {
+                featureType: 'water',
+                elementType: 'geometry.fill',
+                stylers: [{color: '#b9d3c2'}]
+                },
+                {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#92998d'}]
+                }
+        ],
+        {name: 'Light'});
+
+    var styledMapDark = new google.maps.StyledMapType(
+        [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#242f3e"
+                }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#746855"
+                }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                {
+                    "color": "#242f3e"
+                }
+                ]
+            },
+            {
+                "featureType": "administrative.locality",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#d59563"
+                }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#d59563"
+                }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#263c3f"
+                }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#6b9a76"
+                }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#38414e"
+                }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                {
+                    "color": "#212a37"
+                }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#9ca5b3"
+                }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#746855"
+                }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                {
+                    "color": "#1f2835"
+                }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#f3d19c"
+                }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#2f3948"
+                }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#d59563"
+                }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#234a56"
+                },
+                {
+                    "saturation": 10
+                }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#515c6d"
+                }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                {
+                    "color": "#17263c"
+                }
+                ]
+            }
+            ],
+            {name: 'Dark '});      
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: user.location,
+        mapTypeControlOptions: {
+            mapTypeIds: ['roadmap', 'styled_light', 'styled_dark']
+        }
+    });
+
+    // Sets styled_map 
+    map.mapTypes.set('styled_light', styledMapLight);
+    map.mapTypes.set('styled_dark', styledMapDark);
+    map.setMapTypeId('styled_dark');
+
+    // Transforms Local Database object into an array containing location objects without usernames
+    //let tempArr = Object.values(dbl);
+    //let locationArr = Object.values(tempArr);   
+    
+    // Shops examples
+    var locat = [];
+    for (const users in dbl) {
+        console.log("Disposing marker for user: "+ users);
+        var add = locat.push({lat: dbl[users].location.lat, lng: dbl[users].location.lng});
+        //console.log (locat);
+    }
+    locat.splice(0, 1);
+    
+    var markers = locat.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            icon : "testicon2.png"
+        })
+    })
+        
+    var markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'node_modules/@google/markerclustererplus/images/m'});
+    
+
+    var marker = new google.maps.Marker({
+        position: user.location,
+        map: map,
+        title: 'That is you!',
+        icon: icons.client
+    });
+}
+
+
+function createMarker(position, map){
+    var zoom = map.getZoom();
+}
+// Geo measure function - returns the distance between two coordinates
+// Haversine formula
+function hav(lat1, lon1, lat2, lon2){  
+    var R = 6378.137; // Radius of earth in KM
+    var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+    var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d * 1000; // Returns the distance in Meters
 }
